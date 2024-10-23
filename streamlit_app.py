bcea52033f260dc024b92d8f5d14d124a2c3cef7
Moonearth0218 <msy8087@gmail.com>
2024-10-23 10:10:33 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-st.title("👋🏻 지구과학1 공지사항")
+st.title("👋🏻 지구과학1 공지사항!!")
 st.subheader("Welcome to the earthscience")
 st.warning("경기여고 교사 문성윤입니다. 지구과학1과 통합과학을 가르쳐요")
 
 st.link_button(" github 페이지 바로가기!", "https://github.com/Moonearth0218")
 
-#st.success("초록색 창")
-#st.error("빨간색 창")
-#st.info("파란색 창")
-#st.warning("노란색 창") # ctrl+/ : 주석처리
+st.success("초록색 창")
+st.error("빨간색 창")
+st.info("파란색 창")
+st.warning("노란색 창") # ctrl+/ : 주석처리
 st.image("https://media.giphy.com/media/l2JhJtetrernvblxS/giphy.gif?cid=790b7611smb0qoqsa8ez6mcjap9k3k1z508cq6qhmw84crpt&ep=v1_gifs_search&rid=giphy.gif&ct=g", caption="Welcome to coding world") 
 
